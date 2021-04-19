9e4427c0d3ecfeb2228e506525ba91b445e81c9d
ajoseph <ajoseph@antonys-air.attlocal.net>
2021-04-19 07:38:38 -0400

    pom.xml cleanup

--- src/main/java/org/jdbctemplatemapper/config/DatabaseConfig.java
+++ src/main/java/org/jdbctemplatemapper/config/DatabaseConfig.java
@@ -2,9 +2,7 @@ package org.jdbctemplatemapper.config;
 
 import javax.sql.DataSource;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
