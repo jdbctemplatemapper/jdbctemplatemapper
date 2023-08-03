23311c5a3b7020091cd9d99aab44f3c12669fe66
ajoseph <ajoseph@192.168.1.70>
2023-08-03 15:35:23 -0400

    Some docs

--- src/main/java/io/github/jdbctemplatemapper/exception/OptimisticLockingException.java
+++ src/main/java/io/github/jdbctemplatemapper/exception/OptimisticLockingException.java
@@ -1,8 +1,8 @@
 package io.github.jdbctemplatemapper.exception;
 
 /**
- * When JdbcTemplateMapper is configured with a property which is used for versioning and the Object
- * has that property, updates with stale data will throw this exception
+ * When JdbcTemplateMapper is configured with a property which is used for versioning 
+ * and the Object has that property, updates with stale data will throw this exception
  *
  * @author ajoseph
  */
