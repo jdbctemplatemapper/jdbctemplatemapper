410245557358c484958e419ad20410bd9d4ccc24
ajoseph <ajoseph@192.168.1.70>
2023-08-03 15:39:24 -0400

    some docs

--- src/main/java/io/github/jdbctemplatemapper/exception/OptimisticLockingException.java
+++ src/main/java/io/github/jdbctemplatemapper/exception/OptimisticLockingException.java
@@ -1,10 +1,9 @@
 package io.github.jdbctemplatemapper.exception;
 
 /**
- * When JdbcTemplateMapper is configured with a property which is used for versioning 
+ * When JdbcTemplateMapper is configured with a version property  
  * and the Object has that property, updates with stale data will throw this exception
- *
- * @author ajoseph
+ * 
  */
 public class OptimisticLockingException extends RuntimeException {
   private static final long serialVersionUID = 1L;
