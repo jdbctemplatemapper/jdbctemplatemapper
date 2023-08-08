7c0f46c758bc77b43e9328cd5a137887638bc56d
ajoseph <ajoseph@192.168.1.70>
2023-08-08 09:05:33 -0400

    some docs

--- src/main/java/io/github/jdbctemplatemapper/core/SelectMapper.java
+++ src/main/java/io/github/jdbctemplatemapper/core/SelectMapper.java
@@ -15,7 +15,7 @@ import io.github.jdbctemplatemapper.exception.MapperException;
  * <pre>
  * Allows generating the select columns string for the model and population of the model from a ResultSet.
  * 
- * selectMapper.getColumnsSql() will provide a string of columns which can be used in the sql Select statement
+ * selectMapper.getColumnsSql() will provide a string of columns which can be used in the sql select statement. 
  * selectMapper.buildModel(resultSet) will return a model populated from the resultSet data.
  * 
  * Makes the code for writing and retrieving relationships less verbose.
