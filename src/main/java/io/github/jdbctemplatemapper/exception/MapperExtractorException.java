0e0da38454cd60d2467b5ba9906fad24a8d96754
ajoseph <ajoseph@192.168.86.21>
2023-09-03 00:36:44 -0400

    delete mapper extractor exception

--- src/main/java/io/github/jdbctemplatemapper/exception/MapperExtractorException.java
+++
@@ -1,9 +0,0 @@
-package io.github.jdbctemplatemapper.exception;
-
-public class MapperExtractorException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-
-    public MapperExtractorException(String message) {
-        super(message);
-    }
-}
