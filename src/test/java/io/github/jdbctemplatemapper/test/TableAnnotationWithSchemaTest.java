4dd3b850e159315ef6035211bbf0c362fff356dc
ajoseph <ajoseph@192.168.1.70>
2023-09-30 19:17:29 -0400

    With Oracle @Table with schema tests passing

--- src/test/java/io/github/jdbctemplatemapper/test/TableAnnotationWithSchemaTest.java
+++ src/test/java/io/github/jdbctemplatemapper/test/TableAnnotationWithSchemaTest.java
@@ -14,14 +14,14 @@ import io.github.jdbctemplatemapper.core.JdbcTemplateMapper;
 import io.github.jdbctemplatemapper.core.Query;
 import io.github.jdbctemplatemapper.core.QueryCount;
 import io.github.jdbctemplatemapper.core.QueryMerge;
-import io.github.jdbctemplatemapper.model.CompanyCatalogSchema2;
-import io.github.jdbctemplatemapper.model.CustomerCatalogSchema1;
-import io.github.jdbctemplatemapper.model.OfficeCatalogSchema2;
-import io.github.jdbctemplatemapper.model.PersonCatalogSchema1;
+import io.github.jdbctemplatemapper.model.CompanySchema2;
+import io.github.jdbctemplatemapper.model.CustomerSchema1;
+import io.github.jdbctemplatemapper.model.OfficeSchema2;
+import io.github.jdbctemplatemapper.model.PersonSchema1;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-public class TableAnnotationWithCatalogTest {
+public class TableAnnotationWithSchemaTest {
 
   @Value("${spring.datasource.driver-class-name}")
   private String jdbcDriver;
@@ -30,11 +30,11 @@ public class TableAnnotationWithCatalogTest {
   @Qualifier("allJdbcTemplateMapper")
   private JdbcTemplateMapper jtm;
 
-
+  
   @Test
-  public void autoId_crud_withCatalogSchema1Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      CustomerCatalogSchema1 customer = new CustomerCatalogSchema1();
+  public void autoId_crud_withSchema1Annotation() {
+    if (!jdbcDriver.contains("mysql")) {
+      CustomerSchema1 customer = new CustomerSchema1();
       customer.setFirstName("john");
       customer.setLastName("doe");
 
@@ -43,8 +43,8 @@ public class TableAnnotationWithCatalogTest {
       customer.setLastName("xyz");
       jtm.update(customer);
 
-      CustomerCatalogSchema1 customer1 =
-          jtm.findById(CustomerCatalogSchema1.class, customer.getCustomerId());
+      CustomerSchema1 customer1 =
+          jtm.findById(CustomerSchema1.class, customer.getCustomerId());
 
       assertNotNull(customer1);
       assertEquals("xyz", customer1.getLastName());
@@ -53,11 +53,11 @@ public class TableAnnotationWithCatalogTest {
       assertEquals(count, 1);
     }
   }
-
+  
   @Test
-  public void nonAutoId_crud_withCatalogSchema1Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      PersonCatalogSchema1 person = new PersonCatalogSchema1();
+  public void nonAutoId_crud_withSchema1Annotation() {
+    if (!jdbcDriver.contains("mysql")) {
+      PersonSchema1 person = new PersonSchema1();
       person.setPersonId("2001");
       person.setFirstName("john");
       person.setLastName("doe");
@@ -67,7 +67,7 @@ public class TableAnnotationWithCatalogTest {
       person.setLastName("xyz");
       jtm.update(person);
 
-      PersonCatalogSchema1 person1 = jtm.findById(PersonCatalogSchema1.class, person.getPersonId());
+      PersonSchema1 person1 = jtm.findById(PersonSchema1.class, person.getPersonId());
       assertNotNull(person1);
       assertEquals("xyz", person1.getLastName());
 
@@ -75,17 +75,17 @@ public class TableAnnotationWithCatalogTest {
       assertEquals(count, 1);
     }
   }
-
-
+  
+  
   @Test
-  public void query_withCatalogSchema1Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      CustomerCatalogSchema1 customer = new CustomerCatalogSchema1();
+  public void query_withSchema1Annotation() {
+    if (!jdbcDriver.contains("mysql")) {
+      CustomerSchema1 customer = new CustomerSchema1();
       customer.setLastName("abc");
 
       jtm.insert(customer);
 
-      List<CustomerCatalogSchema1> list = Query.type(CustomerCatalogSchema1.class)
+      List<CustomerSchema1> list = Query.type(CustomerSchema1.class)
           .where("customer_id = ?", customer.getCustomerId()).execute(jtm);
 
       assertEquals(1, list.size());
@@ -93,82 +93,82 @@ public class TableAnnotationWithCatalogTest {
       jtm.delete(customer);
     }
   }
-
+  
   @Test
-  public void queryCount_withCatalogSchema1Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      CustomerCatalogSchema1 customer = new CustomerCatalogSchema1();
+  public void queryCount_withSchema1Annotation() {
+    if (!jdbcDriver.contains("mysql")) {
+      CustomerSchema1 customer = new CustomerSchema1();
       customer.setLastName("abc");
 
       jtm.insert(customer);
 
-      Integer count = QueryCount.type(CustomerCatalogSchema1.class)
+      Integer count = QueryCount.type(CustomerSchema1.class)
           .where("customer_id = ?", customer.getCustomerId()).execute(jtm);
 
       assertEquals(1, count);
 
-      jtm.deleteById(CustomerCatalogSchema1.class, customer.getCustomerId());
+      jtm.deleteById(CustomerSchema1.class, customer.getCustomerId());
     }
   }
-
-
+  
   @Test
   public void query_WithCatalogSchema2Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      CompanyCatalogSchema2 company = new CompanyCatalogSchema2();
+    if (!jdbcDriver.contains("mysql")) {
+      CompanySchema2 company = new CompanySchema2();
       company.setName("abc");
 
       jtm.insert(company);
 
-      List<CompanyCatalogSchema2> list =
-          Query.type(CompanyCatalogSchema2.class).where("id = ?", company.getId()).execute(jtm);
+      List<CompanySchema2> list =
+          Query.type(CompanySchema2.class).where("id = ?", company.getId()).execute(jtm);
 
       assertEquals(1, list.size());
 
-      jtm.deleteById(CompanyCatalogSchema2.class, company.getId());
+      jtm.deleteById(CompanySchema2.class, company.getId());
 
     }
   }
-
+  
   @Test
-  public void queryCount_withCatalogSchema2Annotation() {
-    if (jdbcDriver.contains("mysql")) {
-      CompanyCatalogSchema2 company = new CompanyCatalogSchema2();
+  public void queryCount_withSchema2Annotation() {
+    if (!jdbcDriver.contains("mysql")) {
+      CompanySchema2 company = new CompanySchema2();
       company.setName("abc");
 
       jtm.insert(company);
 
-      Integer count = QueryCount.type(CompanyCatalogSchema2.class).where("id = ?", company.getId())
+      Integer count = QueryCount.type(CompanySchema2.class).where("id = ?", company.getId())
           .execute(jtm);
 
       assertEquals(1, count);
 
-      jtm.deleteById(CustomerCatalogSchema1.class, company.getId());
+      jtm.deleteById(CustomerSchema1.class, company.getId());
     }
   }
-
+  
+  
   @Test
   public void queryMerge_withCatalogSchema2Annotation() {
-    if (jdbcDriver.contains("mysql")) {
+    if (!jdbcDriver.contains("mysql")) {
 
-      CompanyCatalogSchema2 company = new CompanyCatalogSchema2();
+      CompanySchema2 company = new CompanySchema2();
       company.setName("abc");
 
       jtm.insert(company);
 
-      OfficeCatalogSchema2 office = new OfficeCatalogSchema2();
+      OfficeSchema2 office = new OfficeSchema2();
       office.setAddress("street 1");
       office.setCompanyId(company.getId());
 
       jtm.insert(office);
 
-      List<CompanyCatalogSchema2> companies =
-          Query.type(CompanyCatalogSchema2.class).where("id = ?", company.getId()).execute(jtm);
+      List<CompanySchema2> companies =
+          Query.type(CompanySchema2.class).where("id = ?", company.getId()).execute(jtm);
 
-      QueryMerge.type(CompanyCatalogSchema2.class).hasMany(OfficeCatalogSchema2.class)
+      QueryMerge.type(CompanySchema2.class).hasMany(OfficeSchema2.class)
           .joinColumnManySide("company_id").populateProperty("offices").execute(jtm, companies);
       
     }
   }
-
+  
 }
